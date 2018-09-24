497fa357aac37461e3c272edb5ca284a2db1bac2
ClaireDL <cl.delleluche@gmail.com>
2018-09-24 23:00:00 +0100

    worked on Scala problems till probem 5

--- exercises/ch5_scalaproblems.py
+++ exercises/ch5_scalaproblems.py
@@ -7,21 +7,96 @@ list_1 = [1, 1, 2, 3, 5, 8]
 
 # solution 1
 last_element = list_1[-1]
-print("The last element of the list is %s." % last_element)
+print("The last element of the list is %i." % last_element)
 
 # solution 2
 last_element2 = list_1[len(list_1)-1]
-print("The last element of the list is %s." % last_element2)
+print("The last element of the list is %i." % last_element2)
+
+# solution 3
+
+
+def last_element(xs):
+    if len(xs) == 1:
+        return xs
+    return last_element(xs[1:])
+
+
+final_list_last = last_element(list_1)
+print("The last element of the list is %i." % final_list_last[0])
+
 
 # Problem 2
 #  Find the last but one element of a list
 last_but_one = list_1[-2]
 print("The last but one element of the list is %s." % last_but_one)
 
+# solution 2
+last_element2 = list_1[len(list_1)-2]
+print("The last but one element of the list is %s." % last_element2)
+
+# solution 3
+
+
+def last_element_but_one(xs):
+    if len(xs) == 2:
+        return xs
+    return last_element_but_one(xs[1:])
+
+
+final_list_last_but_one = last_element_but_one(list_1)
+print("The last element but one of the list is %i." % final_list_last_but_one[0])
+
 # Problem 3
 # Find the Kth element of a list.
+
+# solution 1
 kth_element = input("Enter the index number of the element you want: ")
 if kth_element > len(list_1):
     print("This is not a valid index, too big.")
 else:
     print("The Kth element of the list is %i." % list_1[kth_element])
+
+# Problem 4
+# Find the number of elements of a list
+
+# solution 1
+print("The list contains %i elements." % len(list_1))
+
+# Problem 5
+# Reverse a list
+
+# solution 1
+
+
+def reverse_recursive(xs):
+    if xs == []:
+        return []
+    return reverse_recursive(xs[1:]) + [xs[0]]
+
+
+print("The reverse list is: %s" % reverse_recursive(list_1))
+
+# solution 2
+
+
+def reverse_for_loop(xs):
+    list_reverse = []
+    for current_element in xs:
+        list_reverse.insert(0, current_element)
+    return list_reverse
+
+print("The reverse list is: %s" % reverse_for_loop(list_1))
+
+# solution 3
+
+
+def reverse_new_list(xs):
+    new_list = []
+    for i in range(len(xs), 0):
+        index = len(xs) - i - 1
+        new_list[index] = xs[i]
+    return new_list
+
+print(reverse_new_list(list_1))
+
