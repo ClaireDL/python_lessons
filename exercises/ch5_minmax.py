1abfffd96ad20eb98acf58171bbe2e67a40e5c2e
ClaireDL <cl.delleluche@gmail.com>
2018-11-27 23:35:36 +0000

    improved ch5_minmax.py but no general case yet

--- exercises/ch5_minmax.py
+++ exercises/ch5_minmax.py
@@ -3,6 +3,8 @@
 # Given a list of numbers find the min and max without using built-in min() and
 # max()
 
+import operator
+
 list1 = [2, 48, 3, 4, 7, 14, 5, 36, 9]
 
 # Verification, using built-in functions
@@ -23,30 +25,105 @@ print("min is %i and max is %i " % (minimum, maximum))
 #
 def find_maximum(xs):
     """
-    This function compares elements from the list in pairs.
-    THe bigger of the two elements remains and is compared with next element in
-    the list.
+    From an input list, returns the element with the biggest value
+    Returns None for an empty list
     """
-    element = xs[0]
-    for i in range(1, len(list1)):
-        if element > xs[i]:
-            return element
-        element = xs[i]
-    return element
+    if xs == []:
+        return None
+    maximum =  xs[0]
+    for element in xs:
+        if element >= maximum:
+            maximum = element
+    return maximum
 
 def find_minimum(xs):
     """
-    This function compares elements from the list in pairs.
-    The smaller of the two elements remains and is compared with next element in
-    the list.
+    From an input list, returns the element with the smallest value
+    Returns None for an empty list
     """
-    element = xs[0]
-    for i in range(1, len(list1)):
-        if element < xs[i]:
-            return element
-        element = xs[i]
-    return element
+    minimum = None
+    for element in xs:
+        if element is None or element <= minimum:
+            minimum = element
+    return minimum
 
-max = find_maximum(list1)
-min = find_minimum(list1)
-print("min is %i and max is %i " % (min, max))
+def find_value(xs, operator):
+    """
+    From an input list and operator (minimum, maximum)
+    returns the required value
+    Returns None for an empty list
+    """
+    if xs == []:
+        return None
+    value = xs[0]
+
+    for element in xs:
+            if value_type == "minimum":
+                if operator.le(element, value)
+                value = element
+            elif value_type == "maximum":
+                if operator.ge(element, value)
+                value = element
+        return value
+
+
+#
+# Unit test
+#   Requires installation of package unittest2
+#   Run using: python -m unittest ch5_minmax
+#
+
+import unittest
+
+class MaxTest(unittest.TestCase):
+
+    def test_empty_list(self):
+        result = find_maximum([])
+        self.assertEquals(result, None)
+
+    def test_single_element(self):
+        result = find_maximum([1])
+        self.assertEquals(result, 1)
+
+    def test_increasing_list(self):
+        result = find_maximum([1, 2, 3, 4, 5])
+        self.assertEquals(result, 5)
+
+    def test_decreasing_list(self):
+        result = find_maximum([5, 4, 3, 2, 1])
+        self.assertEquals(result, 5)
+
+    def test_updown_list(self):
+        result = find_maximum([1, 2, 3, 2, 1])
+        self.assertEquals(result, 3)
+
+    def test_downup_list(self):
+        result = find_maximum([3, 2, 1, 2, 3 ])
+        self.assertEquals(result, 3)
+
+
+class MinTest(unittest.TestCase):
+
+    def test_empty_list(self):
+        result = find_minimum([])
+        self.assertEquals(result, None)
+
+    def test_single_element(self):
+        result = find_minimum([1])
+        self.assertEquals(result, 1)
+
+    def test_increasing_list(self):
+        result = find_minimum([1, 2, 3, 4, 5])
+        self.assertEquals(result, 1)
+
+    def test_decreasing_list(self):
+        result = find_minimum([5, 4, 3, 2, 1])
+        self.assertEquals(result, 1)
+
+    def test_updown_list(self):
+        result = find_minimum([1, 2, 3, 2, 1])
+        self.assertEquals(result, 1)
+
+    def test_downup_list(self):
+        result = find_minimum([3, 2, 1, 2, 3 ])
+        self.assertEquals(result, 1)
