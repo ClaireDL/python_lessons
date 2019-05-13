a5f0d77288e7bc8ad8df4bc4f07ce1834356a1f9
ClaireDL <cl.delleluche@gmail.com>
2019-05-13 20:38:26 +0100

    included mergesort execution time to ch7_execution_time.py to generate .txt output

--- exercises/chapter_based/ch7_execution_time_file.py
+++ exercises/chapter_based/ch7_execution_time_file.py
@@ -14,24 +14,46 @@ def generate_random_list(size):
     """
     return [random.randint(0, 100) for i in range(size)]
 
-samples_sizes = [x * x for x in range(50)]
+def output(program, samples_sizes):
+    """
+    Returns output with sample size and execution time
+    """
+    timedata = []
+    for sample_size in samples_sizes:
+        sample = generate_random_list(sample_size)
+        #time_pre = time()
+        time_pre = clock()
+        sample_sorted = program(sample)
+        #time_post = time()
+        time_post = clock()
+        execution_time = (clock() - time_pre)
+        timedata.append([sample_size, execution_time])
+    return timedata
 
-timedata = []
-for sample_size in samples_sizes:
-    sample = generate_random_list(sample_size)
-    #time_pre = time()
-    time_pre = clock()
-    sample_sorted = insertsort(sample)
-    #time_post = time()
-    time_post = clock()
-    execution_time = (clock() - time_pre)
-    timedata.append([sample_size, execution_time])
+# with insert sort
+test_sample = [x * x for x in range(50)]
+output_insertsort = output(insertsort, test_sample)
 
 with open("insertsort.txt", "w+") as result_insertsort:
     result_insertsort.writelines("Insert sort\n")
+
     result_insertsort.writelines("sample size\texecutiontime\n")
-    for line in timedata:
+    for line in output_insertsort:
         result_insertsort.writelines("%i\t%f\n" % (line[0], line[1]))
 
 with open("insertsort.txt", "r") as result_insertsort:
     print(result_insertsort.read())
+
+# with merge sort
+test_sample = [x * x for x in range(50)]
+output_mergesort = output(mergesort, test_sample)
+
+with open("mergesort.txt", "w+") as result_mergesort:
+    result_mergesort.writelines("merge sort\n")
+
+    result_mergesort.writelines("sample size\texecutiontime\n")
+    for line in output_mergesort:
+        result_mergesort.writelines("%i\t%f\n" % (line[0], line[1]))
+
+with open("mergesort.txt", "r") as result_mergesort:
+    print(result_mergesort.read())
