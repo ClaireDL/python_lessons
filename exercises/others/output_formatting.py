c84f954fec8815e4eb9478c7c42fb19f57e3f4a3
Claire Delle Luche <cl.delleluche@gmail.com>
2019-06-03 23:20:31 +0100

    corrected errors in output_formatting.py

--- exercises/others/output_formatting.py
+++ exercises/others/output_formatting.py
@@ -5,33 +5,34 @@ import csv
 import requests
 
 
-def test(json_file, comparison, entry):
+def test(input_file, comparison, entry):
     # selects values of the test that have to be exported
     output = []
     output.append(comparison)
-    expected_return = json_file[comparison][entry]["performance"]["expected_return"]
+    expected_return = input_file[comparison][entry]["performance"]["expected_return"]
     output.append(expected_return)
-    sharpe_ratio = json_file[comparison][entry]["performance"]["sharpe_ratio"]
+    sharpe_ratio = input_file[comparison][entry]["performance"]["sharpe_ratio"]
     output.append(sharpe_ratio)
-    volatility = json_file[comparison][entry]["performance"]["volatility"]
+    volatility = input_file[comparison][entry]["performance"]["volatility"]
     output.append(volatility)
-    return output
 
-def header():
-    csv_file.write("portfolio,")
-    csv_file.write("2p_expected_return,")
-    csv_file.write("2p_sharpe_ratio,")
-    csv_file.write("2p_volatility,")
-    csv_file.write("4p_expected_return,")
-    csv_file.write("4p_sharpe_ratio,")
-    csv_file.write("4p_volatility,")
-    csv_file.write("maxS_expected_return,")
-    csv_file.write("maxS_sharpe_ratio,")
-    csv_file.write("maxS_volatility,")
-    csv_file.write("minV_expected_return,")
-    csv_file.write("minV_sharpe_ratio,")
-    csv_file.write("minV_volatility,")
-    csv_file.write('\n')
+    output_csv = ",".join(map(str, output))
+
+    return output_csv
+
+def header(name):
+    name.write("portfolio,")
+
+    header_efficient_risk_2p = "2p_expected_return, 2p_sharpe_ratio, 2p_volatility,"
+    header_efficient_risk_4p = "4p_expected_return, 4p_sharpe_ratio, 4p_volatility,"
+    header_max_sharpe = "maxS_expected_return, maxS_sharpe_ratio, maxS_volatility,"
+    header_min_volatility = "minV_expected_return, minV_sharpe_ratio, minV_volatility"
+
+    name.write(header_efficient_risk_2p)
+    name.write(header_efficient_risk_4p)
+    name.write(header_max_sharpe)
+    name.write(header_min_volatility)
+    name.write('\n')
 
 
 
@@ -40,18 +41,18 @@ with open("C:/Users/chdell/Downloads/result-3funds-ema_historical_return-exp_cov
     data = json.load(json_file)
     with open("result.csv", "w+") as csv_file:
         # adds header
-        header()
+        header(csv_file)
 
         # collects values in the portfolios and writes the output
         for portfolio in data:
             result = test(data, portfolio, "efficient_risk_2p")
-            json.dump(result, csv_file, separators = (",", ":"))
+            csv_file.write(result)
             result = test(data, portfolio, "efficient_risk_4p")
-            json.dump(result, csv_file, separators = (",", ":"))
+            csv_file.write(result)
             result = test(data, portfolio, "max_sharpe")
-            json.dump(result, csv_file, separators = (",", ":"))
+            csv_file.write(result)
             result = test(data, portfolio, "min_volatility")
-            json.dump(result, csv_file, separators = (",", ":"))
+            csv_file.write(result)
             csv_file.write('\n')
 
 # opens the output file
