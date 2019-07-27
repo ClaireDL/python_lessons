3c4b776b4ccf35f94c32bf17e41de9d5170d30b4
ClaireDL <cl.delleluche@gmail.com>
2019-07-27 10:35:41 +0100

    modified ch7_get_files.py to get more than 1 keyword

--- exercises/chapter_based/ch7_get_files.py
+++ exercises/chapter_based/ch7_get_files.py
@@ -5,23 +5,35 @@ import argparse
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
+# parser.add_argument(
+#     '--keyword', '-k',
+#     default=".py",
+#     type=str,
+#     help="The keyword in the files you want to retrieve"
+# )
+
 parser.add_argument(
-    '--keyword', '-k',
+    '--nkeyword', '-n',
     default=".py",
+    nargs = '+',
     type=str,
-    help="The keyword in the files you want to retrieve"
+    help="All The keywords in the files you want to retrieve"
 )
 parser.add_argument(
     '--path', '-p',
-    default="C:/Users/chdell/Dropbox/Fabrizio&Claire/python_lessons",
+    default="C:/Users/chdell/Dropbox/Fabrizio&Claire/python_lessons/exercises/chapter_based",
     type=str,
     help="The path to the directory you are searching in"
 )
 args = parser.parse_args()
 
-directory = os.chdir(args.path)
+# directory = os.chdir(args.path)
 
 print("You are looking in %s" % args.path)
-print("Files that contain %s: " % args.keyword)
-for file in glob.glob("*%s*" % args.keyword):
-    print(file)
+#print("Files that contain %s: " % args.keyword)
+list_keywords = args.nkeyword
+for entry in list_keywords:
+    for file in glob.glob("*%s*" % entry):
+        print(file)
+#for file in glob.glob("*%s*" % args.keyword):
+#    print(file)
